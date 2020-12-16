69d59a2f03469b871bbdd25aa6b58715a5501c92
Harry Prior <harryprior@hotmail.co.uk>
2020-12-16 13:57:05 +0000

    Comments

--- WikEpubLib/CreateDocs/GetContentXml.cs
+++ WikEpubLib/CreateDocs/GetContentXml.cs
@@ -9,6 +9,13 @@ using WikEpubLib.Records;
 
 namespace WikEpubLib.CreateDocs
 {
+    /// <summary>
+    /// Creates the content file required by the epub format. 
+    /// </summary>
+    /// <remarks>
+    /// The content file uniquly identifies the contents of the epub file, such as the html page, and the images, 
+    /// and contains information regarding their relative directories, and media-type. 
+    /// </remarks>
     public class GetContentXml : IGetContentXml
     {
         public async Task<(XmlType, XDocument)> From(IEnumerable<WikiPageRecord> wikiPageRecords, string bookTitle)
@@ -24,38 +31,32 @@ namespace WikEpubLib.CreateDocs
                    );
 
                 var xmlns = XNamespace.Xmlns + "dc";
-                XNamespace purl = "http://purl.org/dc/elements/1.1/";
+                XNamespace purlNs = "http://purl.org/dc/elements/1.1/";
 
-                // TODO redo default namespace for this seciton without repeates
                 XElement metadata =
                     new XElement(defaultNs + "metadata",
-                        new XElement(purl + "title", bookTitle, new XAttribute(xmlns, purl)),
-                        new XElement(purl + "publisher", "Wikipedia", new XAttribute(xmlns, purl)),
-                        new XElement(purl + "date",
-                            $"{DateTime.Now.Day}-{DateTime.Now.Month}-{DateTime.Now.Year}", new XAttribute(xmlns, purl)),
-                        new XElement(purl + "creator", "Harry Prior", new XAttribute(xmlns, purl)),
+                        new XElement(purlNs + "title", bookTitle, new XAttribute(xmlns, purlNs)),
+                        new XElement(purlNs + "publisher", "Wikipedia", new XAttribute(xmlns, purlNs)),
+                        new XElement(purlNs + "date",
+                            $"{DateTime.Now.Day}-{DateTime.Now.Month}-{DateTime.Now.Year}", new XAttribute(xmlns, purlNs)),
+                        new XElement(purlNs + "creator", "Harry Prior", new XAttribute(xmlns, purlNs)),
                         new XElement(defaultNs + "meta", new XAttribute("name", "cover"), new XAttribute("content", "cover-image"))
                     );
 
                 XElement manifest =
                             new XElement(defaultNs + "manifest");
-
                 manifest.Add(itemElement(defaultNs, "cover", "cover.html", "application/xhtml+xml"));
-
                 manifest.Add(itemElement(defaultNs, "ncxtoc", "toc.ncx", "application/dtbncx+xml"));
 
+                // id of image is arbitrary. href is given by record src map, which also provides the file type
                 int imageId = 1;
                 string GetImageId() => $"image_{imageId++}";
                 foreach (var record in wikiPageRecords)
                 {
                     manifest.Add(itemElement(defaultNs, record.Id, $"{record.Id}.html", "application/xhtml+xml"));
                     if (record.SrcMap is not null)
-                    {
                         foreach (var dictItem in record.SrcMap)
-                        {
                             manifest.Add(itemElement(defaultNs, GetImageId(), dictItem.Value, $"image/{dictItem.Value.Split('.').Last().ToLower()}"));
-                        }
-                    }
                 }
 
                 XElement spine = new XElement(defaultNs + "spine", new XAttribute("toc", "ncxtoc"));
