a7ca67173e225fa7495f30f43685f047b7c93440
Harry Prior <harryprior@hotmail.co.uk>
2021-02-27 11:59:59 +0000

    Create mock impl of HtmlInput class to ensure homogenous html for testing

---
+++ WikEpubLibTests/GetEpubTest/MockHtmlInput.cs
@@ -0,0 +1,30 @@
+﻿using WikEpubLib.Interfaces; 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WikEpubLib;
+using HtmlAgilityPack;
+
+namespace WikEpubLibTests.GetEpubTest
+{
+    public class MockHtmlInput : IHtmlInput
+    {
+        private string _htmlString;
+
+        public MockHtmlInput(string htmlString) 
+        {
+            _htmlString = htmlString;
+        }
+
+        public Task<HtmlDocument[]> GetHtmlDocumentsFromAsync(IEnumerable<string> urls, HtmlWeb htmlWeb)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(_htmlString);
+            var htmlDocuments = new HtmlDocument[1];
+            htmlDocuments[0] = htmlDoc;
+            return Task.Run(() => htmlDocuments);
+        }
+    }
+}
